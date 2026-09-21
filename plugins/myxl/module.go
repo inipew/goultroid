aa965a557bf5f7f49a814a4d49d3959ccac0f0c8
inipew <130418272+inipew@users.noreply.github.com>
2026-09-21 23:07:56 +0700

    refactor(myxl): drop legacy assistant menu wiring

--- plugins/myxl/module.go
+++ plugins/myxl/module.go
@@ -43,9 +43,6 @@ func (m ModuleType) Register(ctx context.Context, rt *module.Runtime) error {
 	if rt.CallbackStore != nil {
 		p.SetStateStore(rt.CallbackStore)
 	}
-	if rt.LegacyAssistantMenu != nil {
-		p.SetAssistantMenu(rt.LegacyAssistantMenu)
-	}
 	return rt.RegisterPlugin(ctx, m.Manifest(), p)
 }
 
